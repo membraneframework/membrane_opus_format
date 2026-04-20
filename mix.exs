84211d61dc26c7ff2a38a381b6bde763ec8c2c2d
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-04-20 14:06:22 +0200

    Update mix.lock

--- mix.exs
+++ mix.exs
@@ -32,19 +32,21 @@ defmodule Membrane.Opus.Format.Mixfile do
 
   defp deps do
     [
-      {:credo, ">= 0.0.0", runtime: false},
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false}
+      {:credo, "~> 1.7", runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.4", only: :dev, runtime: false}
     ]
   end
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling]
+      flags: [:error_handling],
+      plt_add_apps: [:syntax_tools]
     ]
 
     if System.get_env("CI") == "true" do
       # Store PLTs in cacheable directory for CI
+      File.mkdir_p!(Path.join([__DIR__, "priv", "plts"]))
       [plt_local_path: "priv/plts", plt_core_path: "priv/plts"] ++ opts
     else
       opts
