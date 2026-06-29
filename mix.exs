4d3a2f7ef7b64e1c5c9ce4c78523f5a2d106ac4e
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 12:13:02 +0200

    Rename prepend_llms_links to append_llms_links

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Membrane.Opus.Format.Mixfile do
       source_url: @github_url,
       homepage_url: "https://membraneframework.org",
       docs: docs(),
-      aliases: [docs: ["docs", &prepend_llms_links/1]]
+      aliases: [docs: ["docs", &append_llms_links/1]]
     ]
   end
 
@@ -73,7 +73,7 @@ defmodule Membrane.Opus.Format.Mixfile do
     ]
   end
 
-  defp prepend_llms_links(_) do
+  defp append_llms_links(_args) do
     output_dir = docs()[:output] || "doc"
     path = Path.join(output_dir, "llms.txt")
 
